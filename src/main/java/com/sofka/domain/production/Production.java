57b7a1b9877122e48765eb03bcfa69d3ba31abff
Carlos Romero <caaromerogi@unal.edu.co>
2022-08-19 23:12:48 -0500

    Production Aggregate completed

--- src/main/java/com/sofka/domain/production/Production.java
+++ src/main/java/com/sofka/domain/production/Production.java
@@ -3,12 +3,14 @@ package com.sofka.domain.production;
 import co.com.sofka.domain.generic.AggregateEvent;
 
 import co.com.sofka.domain.generic.DomainEvent;
-import com.sofka.domain.production.events.ProductionCreated;
-import com.sofka.domain.production.values.ProductionId;
+import com.sofka.domain.generics.Name;
+import com.sofka.domain.production.events.*;
+import com.sofka.domain.production.values.*;
 import com.sofka.domain.song.values.SongId;
 
 
 import java.util.List;
+import java.util.Objects;
 
 public class Production extends AggregateEvent<ProductionId> {
 
@@ -32,4 +34,63 @@ public class Production extends AggregateEvent<ProductionId> {
         events.forEach(production::applyEvent);
         return production;
     }
+
+    public void addProducer(ProducerId producerId, Name name, Sign sign){
+        Objects.requireNonNull(producerId);
+        Objects.requireNonNull(name);
+        Objects.requireNonNull(sign);
+        appendChange(new ProducerAdded(producerId, name, sign));
+    }
+
+    public void addRecorder(RecorderId recorderId, Volume volume, Brand brand){
+        Objects.requireNonNull(recorderId);
+        Objects.requireNonNull(volume);
+        Objects.requireNonNull(brand);
+        appendChange(new RecorderAdded(recorderId, volume, brand));
+    }
+
+    public void addMasterizer(MasterizerId masterizerId, Equalizer equalizer, Compressor compressor){
+        Objects.requireNonNull(masterizerId);
+        Objects.requireNonNull(equalizer);
+        Objects.requireNonNull(compressor);
+        appendChange(new MasterizerAdded(masterizerId, equalizer, compressor));
+    }
+
+    public void updateProducerName(ProducerId producerId, Name name){
+        Objects.requireNonNull(producerId);
+        Objects.requireNonNull(name);
+        appendChange(new ProducerNameUpdated(producerId, name));
+    }
+
+    public void changeProducerSign(ProducerId producerId, Sign sign){
+        Objects.requireNonNull(producerId);
+        Objects.requireNonNull(sign);
+        appendChange(new ProducerSignChanged(producerId,sign));
+    }
+
+    public void adjustRecorderVolume(RecorderId recorderId, Volume volume){
+        Objects.requireNonNull(recorderId);
+        Objects.requireNonNull(volume);
+        appendChange(new RecorderVolumeAdjusted(recorderId, volume));
+    }
+
+    public void changeRecorderBrand(RecorderId recorderId, Brand brand){
+        Objects.requireNonNull(recorderId);
+        Objects.requireNonNull(brand);
+        appendChange(new RecorderBrandChanged(recorderId, brand));
+    }
+
+    public void changeEqualizer(MasterizerId masterizerId, Equalizer equalizer){
+        Objects.requireNonNull(masterizerId);
+        Objects.requireNonNull(equalizer);
+        appendChange(new EqualizerChanged(masterizerId, equalizer));
+    }
+
+    public void changeCompressor(MasterizerId masterizerId, Compressor compressor){
+        Objects.requireNonNull(masterizerId);
+        Objects.requireNonNull(compressor);
+        appendChange(new CompressorChanged(masterizerId, compressor));
+    }
+
+
 }
